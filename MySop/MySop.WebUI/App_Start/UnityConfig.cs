c20ed5f9177267ed856c87c16aa28537ea74eb2a
didaccs <didac.cs@gmail.com>
2017-12-20 19:14:13 +0100

    Implemented SQL repository

--- MySop/MySop.WebUI/App_Start/UnityConfig.cs
+++ MySop/MySop.WebUI/App_Start/UnityConfig.cs
@@ -3,6 +3,7 @@ using Unity;
 using MyShop.Core.Contracts;
 using MyShop.Core.Models;
 using MyShop.DataAccess.InMemory;
+using MyShop.DataAcces.SQL;
 
 namespace MySop.WebUI
 {
@@ -44,8 +45,8 @@ namespace MySop.WebUI
 
             // TODO: Register your type's mappings here.
             // container.RegisterType<IProductRepository, ProductRepository>();
-            container.RegisterType<IRepository<Product>, InMemoryRepository<Product>>();
-            container.RegisterType<IRepository<ProductCategory>, InMemoryRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
+            container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
         }
     }
 }
